5aa053622751ab2fecca62ef3189241791e095ca
CleberAP <laboratorio@LaboratorioA19.labifms.edu.br>
2018-06-07 22:27:44 -0300

    First Commit
    Make this file for trainning on Class of GitHub.
    
    Add IFMS ON line 7

--- src/semanameioambiente2018sem1github/SemanaMeioAmbiente2018Sem1GitHub.java
+++ src/semanameioambiente2018sem1github/SemanaMeioAmbiente2018Sem1GitHub.java
@@ -4,7 +4,7 @@ public class SemanaMeioAmbiente2018Sem1GitHub {
 
    public static void main(String[] args) {
         System.out.println("Olá mundo...!!!...");
-        System.out.println("Semana do meio ambiente");
+        System.out.println("Semana do meio ambiente IFMS");
         System.out.println("2018");
     }
     
