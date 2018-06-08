8f2a0e7183b1436ac949954c764b4d6bf1b9e152
CleberAP <>
2018-06-07 22:21:09 -0300

    First Commit
    Make this file for trainning on Class of GitHub.

---
+++ src/semanameioambiente2018sem1github/SemanaMeioAmbiente2018Sem1GitHub.java
@@ -0,0 +1,11 @@
+package semanameioambiente2018sem1github;
+
+public class SemanaMeioAmbiente2018Sem1GitHub {
+
+   public static void main(String[] args) {
+        System.out.println("Olá mundo...!!!...");
+        System.out.println("Semana do meio ambiente");
+        System.out.println("2018");
+    }
+    
+}
